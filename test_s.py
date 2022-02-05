5b84c35aebd80d8709169b688a9075b097c4f84e
AlexKitIt <alexkitit8@gmail.com>
2022-02-05 12:44:31 +0400

    fd

--- test_s.py
+++ test_s.py
@@ -7,3 +7,4 @@ def test_yandex_search(browser):
     yandex_main_page.click_on_the_search_button()
     elements = yandex_main_page.check_navigation_bar()
     assert "Картинки" and "Видео" in elements
+    print()
